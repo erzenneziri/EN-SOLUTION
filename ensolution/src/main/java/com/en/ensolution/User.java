84990af26a869f85da429567633c7c3f70fa1439
Erzen Neziri <erzen.neziri@gmail.com>
2025-10-05 17:03:55 +0200

    removed dummy comment

--- ensolution/src/main/java/com/en/ensolution/User.java
+++ ensolution/src/main/java/com/en/ensolution/User.java
@@ -10,7 +10,7 @@ public class User {
     private Long id;
 
     private String name;
-    private String surname;//TEST
+    private String surname;
     private String email;
 
     public User() {}
